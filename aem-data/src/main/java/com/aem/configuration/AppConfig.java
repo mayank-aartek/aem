2f8bdbf2827b92421e13d212d43e49d80b8cba26
Ashish Birle <birleashish@gmail.com>
2016-07-18 11:35:49 +0530

    appconfig

--- aem-data/src/main/java/com/aem/configuration/AppConfig.java
+++ aem-data/src/main/java/com/aem/configuration/AppConfig.java
@@ -46,7 +46,7 @@ public class AppConfig extends WebMvcConfigurerAdapter{
 	@Override
 
 	   public void addViewControllers(ViewControllerRegistry registry) {
-	     registry.addViewController("/").setViewName("login");
+	     registry.addViewController("/").setViewName("home");
 	   }
 
 	
