3f4f841a0ab12f5af11dedd909629dc1deac1206
Ashish Birle <birleashish@gmail.com>
2016-08-02 10:59:31 +0530

    maintainance controller

--- aem-data/src/main/java/com/aem/model/Login.java
+++ aem-data/src/main/java/com/aem/model/Login.java
@@ -1,95 +1,126 @@
 package com.aem.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.*;
+
 @Entity
-public class Login implements Serializable
-{
+public class Login implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="ID")
-     private Integer Id;
-	@Column(name="EMAIL")
-	 private String email;
-	@Column(name="PASSWORD")
-	 private String Password;
-	@Column(name="ENCRYPTED")
-	 private String	encrypted;
-	@Column(name="CREATION_TIME")
-	 private String creationTime;
-	@Column(name="UPDATION_TIME")
-	 private String updationTime;
-	@Column(name="IS_DELETED")
-	 private String isDeleted;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
+	private Integer Id;
+	@Column(name = "EMAIL")
+	private String email;
+	@Column(name = "PASSWORD")
+	private String Password;
+	@Column(name = "ENCRYPTED")
+	private String encrypted;
+	@Column(name = "CREATION_TIME")
+	private String creationTime;
+	@Column(name = "UPDATION_TIME")
+	private String updationTime;
+	@Column(name = "IS_DELETED")
+	private String isDeleted;
 	@Transient
-	 private String oldPassword;
+	private String oldPassword;
 	@Transient
-	 private String newPassword;
+	private String newPassword;
 	@Transient
-	 private String confirmPassword;
+	private String confirmPassword;
+
+	@OneToMany(fetch = FetchType.LAZY, targetEntity = TaskManagement.class, cascade = CascadeType.ALL)
+	@JoinColumn(name = "EMPLOYEE_ID", referencedColumnName = "ID")
+	List<TaskManagement> taskManagement;
+
+	public List<TaskManagement> getTaskManagement() {
+		return taskManagement;
+	}
+
+	public void setTaskManagement(List<TaskManagement> taskManagement) {
+		this.taskManagement = taskManagement;
+	}
+
 	public Integer getId() {
 		return Id;
 	}
+
 	public void setId(Integer id) {
 		Id = id;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return Password;
 	}
+
 	public void setPassword(String password) {
 		Password = password;
 	}
+
 	public String getEncrypted() {
 		return encrypted;
 	}
+
 	public void setEncrypted(String encrypted) {
 		this.encrypted = encrypted;
 	}
+
 	public String getCreationTime() {
 		return creationTime;
 	}
+
 	public void setCreationTime(String creationTime) {
 		this.creationTime = creationTime;
 	}
+
 	public String getUpdationTime() {
 		return updationTime;
 	}
+
 	public void setUpdationTime(String updationTime) {
 		this.updationTime = updationTime;
 	}
+
 	public String getIsDeleted() {
 		return isDeleted;
 	}
+
 	public void setIsDeleted(String isDeleted) {
 		this.isDeleted = isDeleted;
 	}
+
 	public String getOldPassword() {
 		return oldPassword;
 	}
+
 	public void setOldPassword(String oldPassword) {
 		this.oldPassword = oldPassword;
 	}
+
 	public String getNewPassword() {
 		return newPassword;
 	}
+
 	public void setNewPassword(String newPassword) {
 		this.newPassword = newPassword;
 	}
+
 	public String getConfirmPassword() {
 		return confirmPassword;
 	}
+
 	public void setConfirmPassword(String confirmPassword) {
 		this.confirmPassword = confirmPassword;
 	}
-	
-	
-	
+
 }
